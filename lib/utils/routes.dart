3b2e70409bd98815861253cfaf33706b3cf9f697
kaungsatthe1n <parrotksh@gmail.com>
2021-12-24 21:35:07 +0630

    Removed Route names of Nointernet and VideoPlayer

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -2,8 +2,6 @@ class Routes {
   static const String homeScreen = '/';
   static const String searchScreen = '/search';
   static const String videoListScreen = '/video-list';
-  static const String videoPlayerScreen = '/video-player';
-  static const String noInternetScreen = '/no-internet';
   static const String aboutAppScreen = '/about-app';
   static const String mediaFetchScreen = '/media-fetch';
   static const String webViewScreen = '/web-view';
