ab589ca8a81182478e6c0d5369c685235c6b5367
kaungsatthe1n <parrotksh@gmail.com>
2022-01-07 14:05:40 +0630

    Cache Manager for CacheNetworkImage and WebViewProvider Added

---
+++ lib/helpers/cache_manager.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+
+class CustomCacheManager {
+  static const key = 'customCacheKey';
+  static CacheManager instance = CacheManager(
+    Config(
+      key,
+      stalePeriod: const Duration(days: 7),
+      maxNrOfCacheObjects: 120,
+      repo: JsonCacheInfoRepository(databaseName: key),
+      fileService: HttpFileService(),
+    ),
+  );
+}
---
+++ lib/helpers/webview_provider.dart
@@ -0,0 +1,30 @@
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class WebViewController extends GetxController {
+  bool webView = false;
+  bool get isWebView => webView;
+
+  void changeWebViewType(value) {
+    webView = value;
+    updateVideoPlayerType(value);
+    update();
+  }
+
+  Future<void> getVideoPlayerType() async {
+    final pref = await SharedPreferences.getInstance();
+    final isWebView = pref.getBool('isWebViewPlayer');
+    if (isWebView != null) {
+      webView = isWebView;
+      update();
+    } else {
+      webView = false;
+      update();
+    }
+  }
+
+  Future<void> updateVideoPlayerType(flag) async {
+    final pref = await SharedPreferences.getInstance();
+    pref.setBool('isWebViewPlayer', flag);
+  }
+}
