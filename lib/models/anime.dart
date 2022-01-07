4a9511e02433ec0ea9ae7d998a161bb5b9b79096
kaungsatthe1n <parrotksh@gmail.com>
2022-01-07 14:06:07 +0630

    Anime Model Updated

--- lib/models/anime.dart
+++ lib/models/anime.dart
@@ -4,17 +4,27 @@ class AnimeResults {
 }
 
 class Anime {
-  String? id; 
+  String? id;
   String? name;
   String? imageUrl;
-  String? episodeUrl;
+  String? summary;
+  String? status;
+  String? animeUrl;
+  List<String>? genres;
+  List<String>? epLinks;
   String? currentEp;
+  String? releasedDate;
 
   Anime({
     this.id,
     this.name,
     this.imageUrl,
+    this.status,
+    this.summary,
+    this.genres,
+    this.epLinks,
     this.currentEp,
-    this.episodeUrl,
+    this.animeUrl,
+    this.releasedDate,
   });
 }
