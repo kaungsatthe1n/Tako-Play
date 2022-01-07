b3e5fc47e1af63a3a1711e1d65a42f0eb0abbeb6
kaungsatthe1n <parrotksh@gmail.com>
2022-01-07 14:16:38 +0630

    Routes Updated

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,8 +1,11 @@
 class Routes {
-  static const String homeScreen = '/';
+  static const String mainScreen = '/';
+  static const String homeScreen = '/home';
   static const String searchScreen = '/search';
   static const String videoListScreen = '/video-list';
   static const String aboutAppScreen = '/about-app';
   static const String mediaFetchScreen = '/media-fetch';
+  static const String bookMarkScreen = '/bookmarks';
   static const String webViewScreen = '/web-view';
+  static const String videoPlayerScreen = '/video-player';
 }
