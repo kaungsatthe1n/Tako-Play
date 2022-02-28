111d8a68c367895958def196dc87d6e89ef56303
David Djordjevic <david.djordjevic@primeholding.com>
2022-02-28 20:59:48 +0100

    - Update package dependencies - Update ios icon and min iOS version - Implement try again button on no internet screen

--- lib/helpers/network_manager.dart
+++ lib/helpers/network_manager.dart
@@ -1,47 +1,47 @@
 import 'dart:async';
+
 import 'package:connectivity/connectivity.dart';
 import 'package:get/get.dart';
 
+/// [NetworkManager] is used for handling online connectivity state of the app.
+/// It provides an easy way of checking whether the device is online or not
 class NetworkManager extends GetxController {
   final Connectivity _connectivity = Connectivity();
   bool _isOnline = false;
 
+  /// State indicating online connection
   bool get isOnline => _isOnline;
 
   @override
   void onInit() {
     super.onInit();
-    startMornitoring();
+    _startMonitoring();
   }
 
-  Future<void> startMornitoring() async {
-    _connectivity.onConnectivityChanged.listen((result) {
-      if (result == ConnectivityResult.none) {
-        _isOnline = false;
-
-        update();
-      } else {
-        _isOnline = true;
-
-        update();
-      }
-    });
+  /// Manually checks whether the app is connected to internet
+  Future<void> checkConnection() async {
+    final connectivityResult = await _connectivity.checkConnectivity();
+    _onNetworkStateUpdated(connectivityResult);
   }
 
-  // Future<void> initConnectivity() async {
-  //   try {
-  //     var status = await _connectivity.checkConnectivity();
+  /// Starts monitoring the actual network connection
+  Future<void> _startMonitoring() async {
+    _connectivity.onConnectivityChanged.listen(_onNetworkStateUpdated);
 
-  //     if (status == ConnectivityResult.none) {
-  //       _isOnline = false;
-  //       update();
-  //     } else {
-  //       _isOnline = true;
-  //       update();
-  //     }
-  //   } on PlatformException catch (e) {
-  //     // ignore: avoid_print
-  //     print(e.toString());
-  //   }
-  // }
+    // Make the initial connectivity check (just to be sure)
+    checkConnection();
+  }
+
+  /// Callback triggered once there is a change in network connection
+  void _onNetworkStateUpdated(ConnectivityResult result) {
+    if (result == ConnectivityResult.none) {
+      _isOnline = false;
+
+      update();
+    } else {
+      _isOnline = true;
+
+      update();
+    }
+  }
 }
